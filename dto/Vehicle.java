dab756370d19a52f49387d6753e03d62614d6400
SaddiHarivardhanReddy <166787099+SaddiHarivardhanReddy@users.noreply.github.com>
2026-01-07 23:19:05 +0530

    Vehicle dto

---
+++ dto/Vehicle.java
@@ -0,0 +1,28 @@
+package com.codegnan.dto;
+
+public class Vehicle {
+
+    private int id;
+    private String brand;
+    private String model;
+    private double price;
+
+    public Vehicle() {}
+
+    public Vehicle(int id, String brand, String model, double price) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    public int getId() { return id; }
+    public String getBrand() { return brand; }
+    public String getModel() { return model; }
+    public double getPrice() { return price; }
+
+    public void setId(int id) { this.id = id; }
+    public void setBrand(String brand) { this.brand = brand; }
+    public void setModel(String model) { this.model = model; }
+    public void setPrice(double price) { this.price = price; }
+}
